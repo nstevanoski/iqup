2b9890a0446550047882fb42041924ceb18b2115
Nikola S <stevanoskinikolaa@gmail.com>
2025-09-06 19:51:25 +0200

    refactor: Update site configuration and author details in layout. Remove unused component files to streamline the project structure. Change site title and description to reflect the new focus on Franchise Management System.

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'Franchise Management System',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+    'iqUP - Franchise Management System',
+  url: 'https://iqup.vercel.app',
 };
